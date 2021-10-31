39a9e101a86399f6cd181f724d720e3266f4307c
thejana123 <thejanalakshan123@gmail.com>
2021-10-31 13:49:15 +0530

    admin_nav

---
+++ admin_navbar.php
@@ -0,0 +1,22 @@
+<!-- 
+<head>
+    <link rel="stylesheet" href="./styles/admin_navbar.css">
+</head>
+
+<body> -->
+<nav class="navbar navbar-expand-md navbar-light" id="navigation">
+    <div class="container-fluid">
+        <a class="navbar-brand" id="brand" href="home.php">
+            <img src="./image/bookgroup2.png" id="logo" width="80px" height="40px" alt="bookstore" />
+            Sarasi Bookstore</a>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav ml-auto" id="navbarItems">
+                <a class="nav-link" aria-current="page" href="home.php">Home</a>
+                <a class="nav-link" aria-current="page" href="admin_profile.php">Admin Profile</a>
+            </div>
+        </div>
+    </div>
+</nav>
+
+
+<script src="js/nav.js"></script>
