529ac4456566c281d53a229c75bd47561adf8e72
thejana123 <thejanalakshan123@gmail.com>
2022-10-30 20:38:28 +0530

    bug update

--- authorDes.php
+++ authorDes.php
@@ -12,15 +12,15 @@ while ($rows = $result->fetch_assoc()) {
     $name = $rows['author_name'];
     $image = $rows['author_image'];
     $dis = $rows['author_description'];
+    $_ENV =$rows['country'];
+
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
     <title>Authors</title>
 
     <link rel="stylesheet" href="./styles/footer.css">
