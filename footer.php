25e79078c03d1c1f110553bf13be0070f844dd7c
Antonio <elguitarraverde@hotmail.com>
2023-10-24 18:31:36 +0200

    updated footer styles

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
     <footer class="container-fluid" id="footer">
         <div class="row">
             <div class="col-12 col-sm-12 col-md-4" id="location">
-                <span>Location</span><br>
+                <span class="title">Location</span><br>
                 <div class="container-fluid" id="address">
                     <span>No.00014</span><br>
                     <span>Polonnaruwa</span><br>
@@ -17,21 +17,21 @@
             </div>
 
             <div class="col-12 col-sm-12 col-md-4" id="socialTags">
-                <span>Follow us on</span>
+                <span class="title">Follow us on</span>
                 <div class="container-fluid" id="social">
                     <div class="row" id="socialRow">
-                        <div class="col-3"><a href="#"><i class="bi bi-facebook"></i></a></div>
-                        <div class="col-3"><a href="#"><i class="bi bi-twitter"></i></a></div>
-                        <div class="col-3"><a href="#"><i class="bi bi-linkedin"></i></a></div>
-                        <div class="col-3"><a href="#"><i class="bi bi-github"></i></a></div>
-                        <div class="col-3"><a href="#"><i class="bi bi-instagram"></i></a></div>
+                        <div class="col-2"><a href="#"><i class="bi bi-facebook"></i></a></div>
+                        <div class="col-2"><a href="#"><i class="bi bi-twitter"></i></a></div>
+                        <div class="col-2"><a href="#"><i class="bi bi-linkedin"></i></a></div>
+                        <div class="col-2"><a href="#"><i class="bi bi-github"></i></a></div>
+                        <div class="col-2"><a href="#"><i class="bi bi-instagram"></i></a></div>
                     </div>
                 </div>
                 <hr class="d-block d-sm-none" id="line">
             </div>
 
             <div class="col-12 col-sm-12 col-md-4" id="helpCentre">
-                <span>Help Centre</span><br>
+                <span class="title">Help Centre</span><br>
                 <div class="container-fluid" id="info">
                     <span>ruchcha@gmail.com</span><br>
                     <span>001111555</span>
